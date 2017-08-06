7e2b350a2d70efe83110b5fe6a8952d76e9d247b
Tyler Gabb <tylerjgabb@gmail.com>
2017-08-06 09:52:14 -0700

    Adding Source

---
+++ Timer Exception Not Caught/StackOverflow/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace StackOverflow
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer T;
+        public Form1()
+        {
+            InitializeComponent();
+            T = new System.Timers.Timer();
+            T.Interval = 5000; //Miliseconds;
+            T.Elapsed += new ElapsedEventHandler(TimerElapsed);
+            T.Start();
+        }
+
+        public void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            T.Stop();
+            throw new Exception("Exception From Timer Elapsed");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            throw new Exception("Exception 1");
+        }
+    }
+}
---
+++ Timer Exception Not Caught/StackOverflow/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace StackOverflow
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(UnhandledExceptionOccurred);
+            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(UnhandledExceptionOccurred);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public static void UnhandledExceptionOccurred(object sender, UnhandledExceptionEventArgs e)
+        {
+            var x = (Exception)e.ExceptionObject;
+            MessageBox.Show("Unhandled Exception\r\n" + x.Message);
+            
+        }
+
+        public static void UnhandledExceptionOccurred(object sender, ThreadExceptionEventArgs e_thread)
+        {
+            var x = e_thread.Exception;
+            MessageBox.Show("Thread Exception\r\n" + x.Message);
+        }
+
+    }
+}
