036b3c6e6988d1e7b9ed25b02b30e8c40762961c
tylerjgabb <tylerjgabb@gmail.com>
2018-03-26 13:36:14 -0700

    changed into generic C# sandbox

---
+++ ConsoleApps/DisposeOverride/DisposeOverride/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisposeOverride
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class SomeDisposableClass : IDisposable
+    {
+        public int IntProp { get; set; }
+        public string StringProp { get; set; }
+        
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~SomeDisposableClass() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # C-Sharp
 C# Code
+This repo started as a holding place for source code for a stack overflow post. 
+Is is now a generic sandbox for any and everything C# abd .NET related 
 
 
 
-Source code for a stack overflow post
+
